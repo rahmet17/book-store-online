60292dbdc2eeeba2371dbf7dd56cb3ffea929353
Akhmetgaraev Ramil <ramil.akhmetgaraev@gmail.com>
2020-12-21 18:49:46 +0300

    Create user, shop, cart sagas

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,33 @@
+import { all, call, takeLatest, put } from 'redux-saga/effects';
+
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+import {
+    fetchCollectionsSuccess,
+    fetchCollectionsFailure
+} from './shop.actions';
+
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollections() {
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+        yield put(fetchCollectionsSuccess(collectionMap));
+    }
+    catch (error) {
+        yield put(fetchCollectionsFailure(error.message));
+    }
+}
+
+export function* fetchCollectionsStart() {
+    yield takeLatest(
+        ShopActionTypes.FETCH_COLLECTIONS_START,
+        fetchCollections
+    );
+}
+
+export function* shopSagas() {
+    yield all([call(fetchCollectionsStart)]);
+}
